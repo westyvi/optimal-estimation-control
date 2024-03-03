d63f9087c639c3f37025c082ebba9f05e07f679e
westyvi <79417323+westyvi@users.noreply.github.com>
2024-03-03 15:37:30 -0600

    add partition data into rest periods step

--- P3_NLS/P3.py
+++ P3_NLS/P3.py
@@ -33,6 +33,8 @@ def load_data_from_csv(file_path):
     with open(file_path, 'r') as file:
         csv_reader = csv.reader(file)
         next(csv_reader)  # Skip header if present
+        time_last = 0
+        day_count = 0
         for row in csv_reader:
             # Assuming the CSV columns are in the order: time, voltage, current
             time_str, voltage_str, current_str = row
@@ -40,8 +42,13 @@ def load_data_from_csv(file_path):
             # Convert time string to a time object
             time_parts = time_str.split(':')
             hours, minutes, seconds = map(int, time_parts)
-            time_seconds = hours * 3600 + minutes * 60 + seconds
+            time_seconds = hours * 3600 + minutes * 60 + seconds + day_count * 24 * 60 * 60
+            # account for change of day, e.g. hour count resets 24 -> 0
+            if time_last>time_seconds:
+                day_count += 1
+                time_seconds += 24*60*60
             times.append(time_seconds)
+            time_last = time_seconds
             
             # Convert voltage and current strings to floats
             voltages.append(float(voltage_str))
@@ -56,20 +63,50 @@ def load_data_from_csv(file_path):
 
 # load data in to arrays
 times, voltages, currents = load_data_from_csv('./pulse_discharge_test_data.csv')
+times -= times[0]
 
-# Integrate current to get state of charge (SOC); assume SOC[0] = 0
-SOC = np.copy(times)
-SOC[0] = 0
+# Integrate current to get state of charge (Q); assume Q[0] = 0
+Q = np.copy(times)
+Q[0] = 0
 time_step = times[1]-times[0]
-SOC = np.cumsum(currents) * -time_step # in joules
+Q = np.cumsum(currents) * - time_step # in joules
+Q -= Q[-1] # find total charge assuming ending Q=0
+SOC = Q/Q[0] # define SOC assuming Q[0] corresponds to 100% SOC
 
-# Assuming you have arrays 'times' and 'state_of_charge'
+# plot Q vs time
 fig, ax = plt.subplots()
-ax.plot(times, SOC)
-ax.set(xlabel = 'Time, s', ylabel = 'State of Charge, J',
+ax.plot(times, SOC*100)
+ax.set(xlabel = 'Time, s', ylabel = 'State of Charge, percent',
       title = 'State of Charge vs Time')
 ax.grid(True)
 
+# break up discharge data into rest periods
+rest_period = []
+amp_threshold = 0.1 # determines whether in a rest period or not
+start = 0
+end = 0
+i = 0
+while i < currents.size:
+    # if found the start of a rest period
+    if currents[i] < amp_threshold:
+        start = i
+        for end in range(i,currents.size):
+            if currents[end] > amp_threshold:
+                break
+        i = end
+        rest_period.append(currents[start:end])
+    i += 1
+            
+
+
+
+
+
+
+
+
+
+# cracked multi case plotting function if you want to use it
 '''# %%plot results
 t = np.linspace(0, simTime, numpts)
 fig1, ax1 = plt.subplots()

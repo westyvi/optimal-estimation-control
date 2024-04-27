44ce964e1af73ddd67c9bc41c271ae874e73ca14
westyvi <79417323+westyvi@users.noreply.github.com>
2024-04-27 14:40:00 -0500

    implement functioning EKF, implement UKF with possible bug, fix some issues with matrix multiplication of vectors in baseKF class for different state and measurement dimensions

--- P5_NonlinearKFs/P5.py
+++ P5_NonlinearKFs/P5.py
@@ -4,7 +4,7 @@
 """
 AEM566 - Optimal Estimation and Control
 Project 5:
-    
+
     Nonlinear Kalman Filtering for ballistic altitude estimation
 
 initial assignment contained in project folder
@@ -24,34 +24,120 @@ csv_data = np.genfromtxt('./altimeter_data.csv', delimiter=',', dtype=None)
 
 # Extract data into numpy arrays
 time = csv_data[:, 0]  # time
-uk = csv_data[:, 1]    # fuel flow meter sensor data 
-yk = csv_data[:, 2]    # fuel tank level sensor data 
-fk = csv_data[:, 3]    # true fuel remaining
-bk = csv_data[:, 4]    # true flow meter bias 
+yk = csv_data[:, 1]    # range measurement
+hk = csv_data[:, 2]    # true altitude
+sk = csv_data[:, 3]    # true vertical velocity
+Cbk = csv_data[:, 4]    # true Ballistic Coefficient
 
 # define model parameters
 dt = 0.5 # seconds
 rho0 = 0.0765 # lb/ft3
 g0 = 32.2 # ft/s2
-h_rho = 30000 # ft
-R_E = 20902260 #ft
-d = 100000 # ft
-Q = np.diag([10**2, 10**2, 0.05**2])
-R = 100**2 # ft2
-x0 = np.array([400000, -2000, 20]) # ft, ft/s, lb/ft2
-P0 = np.diag([100**2, 10**2, 1**2]) # ft2, ft2/s2, 
+h_rho = 30000.0 # ft
+R_E = 20902260.0 #ft
+d = 100000.0 # ft
+Q = np.diag([10.0**2, 10.0**2, .05**2])
+R = 100.0**2 # ft2
+x0 = np.array([400000.0, -2000.0, 20.0]) # ft, ft/s, lb/ft2
+P0 = np.diag([100.0**2, 10.0**2, 1.0**2]) # ft2, ft2/s2, 
 
 # define state space model for fuel system:
 # state vector: [altitude, verticalSpeed, ballistic coefficient]
-def propogate_state(x_hat):
-    f0 = lambda x_hat: x_hat[0] + dt*x_hat[1]
-    f1 = lambda x_hat: x_hat[1] + dt*(rho0*x_hat[1]**2/2/x_hat[2]*np.exp(-x_hat[0]/h_rho) - g0*(R_E/(R_E + x_hat[0]))**2)
-    f2 = lambda x_hat: x_hat[2]
-    return [f0, f1, f2]
+def propogate_state(x_hat, u=0, w=np.zeros(3)):
+    f0 = lambda x_hat: x_hat[0] + dt*x_hat[1] + w[0]
+    f1 = lambda x_hat: x_hat[1] + dt*(rho0*x_hat[1]**2/2/x_hat[2]*np.exp(-x_hat[0]/h_rho) - g0*(R_E/(R_E + x_hat[0]))**2) + w[1]
+    f2 = lambda x_hat: x_hat[2] + w[2]
+    next_state = np.array([f0(x_hat), f1(x_hat), f2(x_hat)])
+    return next_state
 
-i = 1
-def measure(x_hat):
-    pass
+# test the propogate function
+if False:
+    xtest = np.zeros((len(x0), len(time)))
+    xtest[:,0] = x0
+    for i in range(len(time)-1):
+        xtest[:,i+1] = propogate_state(xtest[:,i])
+    fig, ax = plt.subplots()
+    ax.plot(time, xtest[2,:], c='red', label='model')
+    ax.plot(time, Cbk, c='black', label='truth')
+    ax.set(xlabel = 't, s', ylabel = 'Altitude, ft',
+          title = 'Estimated Altitude vs time, ')
+    ax.legend()
+    plt.grid(True)
+
+def gen_state_jacobian(x_hat):
+    
+    F = np.array([[1, dt, 0],
+                  [
+                      dt*(-1/h_rho*rho0*x_hat[1]**2/2/x_hat[2]*np.exp(-x_hat[0]/h_rho) + g0*2*R_E**2/((R_E + x_hat[0])**3)), 
+                      1 + dt*rho0*x_hat[1]/x_hat[2]*np.exp(-x_hat[0]/h_rho),
+                      -dt*rho0*x_hat[1]**2/(2*x_hat[2]**2)*np.exp(-x_hat[0]/h_rho)
+                   ],
+                  [0,0,1]
+        ])
+    # don't want to delete this because it can show very interesting behavior:
+    # if x_hat is integers, the **3 operation goes very wrong, but isn't replicable in terminal
+    '''print(x_hat[0])
+    print( (R_E + x_hat[0]) )
+    print( ((R_E + x_hat[0])**3) )'''
+    return F
+
+# this shows strange integer vs float behavior not replicable in terminal:
+'''F1 = gen_state_jacobian(x0)
+print(F1)
+print('')
+
+xtest = np.zeros((len(x0), len(time)))
+xtest[:,0] = x0
+F2 = gen_state_jacobian(xtest[:,0])
+print(F2)'''
+
+# test propogating just from function
+if False:
+    xtest = np.zeros((len(x0), len(time)))
+    xtest[:,0] = x0
+    for i in range(len(time)-1):
+        F = gen_state_jacobian(xtest[:,i])
+        xtest[:,i+1] = F@xtest[:,i]
+    fig, ax = plt.subplots()
+    ax.plot(time, xtest[0,:], c='red', label='model')
+    ax.plot(time, hk, c='black', label='truth')
+    ax.set(xlabel = 't, s', ylabel = 'Altitude, ft',
+          title = 'Estimated Altitude vs time, ')
+    ax.legend()
+    plt.grid(True)
+    plt.ylim(0, 500000)
+    
+    xtest = np.zeros((len(x0), len(time)))
+    xtest[:,0] = x0
+    for i in range(len(time)-1):
+        F = gen_state_jacobian(xtest[:,i])
+        xtest[:,i+1] = F@xtest[:,i]
+    fig, ax = plt.subplots()
+    ax.plot(time, xtest[1,:], c='red', label='model')
+    ax.plot(time, sk, c='black', label='truth')
+    ax.set(xlabel = 't, s', ylabel = 'Vspeed, ft',
+          title = 'vertical speed vs time, ')
+    ax.legend()
+    plt.grid(True)
+    plt.ylim(0, -4000)
+
+# define measurement model for system
+def measure(x_hat, v=0):
+    return np.sqrt(d**2 + x_hat[0]**2) + v
+
+def gen_measure_jacobian(x_hat):
+    return np.array( [x_hat[0]/np.sqrt(d**2 + x_hat[0]**2), 0, 0] )
+
+def gen_process_noise_jacobian(x_hat):
+    return np.array([
+        [1, 0, 0],
+        [0, 1, 0],
+        [0, 0, 1]
+        ])
+
+def gen_measure_noise_jacobian(x_hat):
+    return np.array([1])
+
 
 # dict to log results of running filters
 filters_log = dict()
@@ -66,14 +152,14 @@ filters_log['BSKF'] = copy.deepcopy(template)
 filters_log['FIKS'] = copy.deepcopy(template)
 
 # add gain logging for EKF, UKF
-filters_log['EKF']['gain'] = np.zeros((time.size-1, 2))
-filters_log['UKF']['gain'] = np.zeros((time.size-1, 2))
+filters_log['EKF']['gain'] = np.zeros((time.size-1, 3))
+filters_log['UKF']['gain'] = np.zeros((time.size-1, 3))
 
 # define function to run filters and log results for data in csv
 def runFilter(filter_object, filter_log, log_gain = False):
     for i in range(0,time.size-1):
         # could make faster by not looking up data in log, but this more readable
-        x_apriori, P_apriori = filter_object.predict(filter_log['x'][i,:], filter_log['P'][i,:,:], dt, uk[i])
+        x_apriori, P_apriori = filter_object.predict(filter_log['x'][i,:], filter_log['P'][i,:,:])
         x_posteriori, P_posteriori = filter_object.correct(yk[i+1], x_apriori, P_apriori)
         filter_log['x'][i+1,:] = x_posteriori
         filter_log['P'][i+1,:,:] = P_posteriori
@@ -84,28 +170,37 @@ def runFilter(filter_object, filter_log, log_gain = False):
 def plot(filter_log, filter_string):
     # Plot the posterior state estimates versus the true states for the fuel remaining.
     fig, ax = plt.subplots()
-    ax.plot(time, filter_log['x'][:,0], c='red', label='estimated')
-    ax.plot(time, fk, c='black', label='truth')
-    ax.set(xlabel = 't, s', ylabel = 'Fuel Remaining, cm^3',
-          title = 'Estimated Fuel Remaining vs time, ' + filter_string)
+    ax.plot(time, filter_log['x'][:,0], c='red', label='estimated', lw=2)
+    ax.plot(time, hk, c='black', label='truth')
+    ax.set(xlabel = 't, s', ylabel = 'Altitude, ft',
+          title = 'Estimated Altitude vs time, ' + filter_string)
     ax.legend()
     plt.grid(True)
     
     # Plot the posterior state estimates versus the true states for the flow meter bias.
     fig, ax = plt.subplots()
     ax.plot(time, filter_log['x'][:,1], c='red', label='estimated')
-    ax.plot(time, bk, c='black', label='truth')
-    ax.set(xlabel = 't, s', ylabel = 'Flowmeter Bias, cm',
-          title = 'Estimated Flowmeter Bias vs time, ' + filter_string)
+    ax.plot(time, sk, c='black', label='truth')
+    ax.set(xlabel = 't, s', ylabel = 'Vertical speed, ft/s',
+          title = 'Estimated Vertical Speed vs time, ' + filter_string)
+    ax.legend()
+    plt.grid(True)
+    
+    # Plot the posterior state estimates versus the true states for the flow meter bias.
+    fig, ax = plt.subplots()
+    ax.plot(time, filter_log['x'][:,2], c='red', label='estimated')
+    ax.plot(time, Cbk, c='black', label='truth')
+    ax.set(xlabel = 't, s', ylabel = 'Ballistic Coefficient',
+          title = 'Estimated Ballistic Coefficient vs time, ' + filter_string)
     ax.legend()
     plt.grid(True)
     
     # Plot the posterior state estimate errors and the ±2𝜎 from the diagonals of the steady-state
     # posterior covariance for the fuel remaining.
     fig, ax = plt.subplots()
-    ax.plot(time, filter_log['x'][:,0]-fk, c='red', label=filter_string)
+    ax.plot(time, filter_log['x'][:,0]-hk, c='red', label=filter_string)
     ax.fill_between(time, -2*np.sqrt(filter_log['P'][:,0,0]), 2*np.sqrt(filter_log['P'][:,0,0]), color='purple', alpha=0.5, label='2-sigma covariance bound')
-    ax.set(xlabel = 't, s', ylabel = 'Fuel Remaining, cm^3',
+    ax.set(xlabel = 't, s', ylabel = 'Altitude',
           title = 'Posterior State Estimate Errors and estimated 2-sigma bounds vs time, ' + filter_string)
     ax.legend()
     plt.grid(True)
@@ -113,25 +208,38 @@ def plot(filter_log, filter_string):
     # Plot the posterior state estimate errors and the ±2𝜎 from the diagonals of the steady-state
     # posterior covariance for the flow meter bias.
     fig, ax = plt.subplots()
-    ax.plot(time, filter_log['x'][:,1]-bk, c='red', label=filter_string)
+    ax.plot(time, filter_log['x'][:,1]-sk, c='red', label=filter_string)
     ax.fill_between(time, -2*np.sqrt(np.abs(filter_log['P'][:,1,1])), 2*np.sqrt(np.abs(filter_log['P'][:,1,1])), color='purple', alpha=0.5, label='2-sigma covariance bound')
-    ax.set(xlabel = 't, s', ylabel = 'Flowmeter Bias, cm',
+    ax.set(xlabel = 't, s', ylabel = 'Vertical Speed',
+          title = 'Posterior State Estimate Errors and estimated 2-sigma bounds vs time, ' + filter_string)
+    ax.legend()
+    plt.grid(True)
+    
+    # Plot the posterior state estimate errors and the ±2𝜎 from the diagonals of the steady-state
+    # posterior covariance for the flow meter bias.
+    fig, ax = plt.subplots()
+    ax.plot(time, filter_log['x'][:,2]-Cbk, c='red', label=filter_string)
+    ax.fill_between(time, -2*np.sqrt(np.abs(filter_log['P'][:,2,2])), 2*np.sqrt(np.abs(filter_log['P'][:,2,2])), color='purple', alpha=0.5, label='2-sigma covariance bound')
+    ax.set(xlabel = 't, s', ylabel = 'Ballistic Coefficient',
           title = 'Posterior State Estimate Errors and estimated 2-sigma bounds vs time, ' + filter_string)
     ax.legend()
     plt.grid(True)
     
 
-# standard discrete-time Kalman Filter (EKF)
-EKF = P4_classes.EKF(F, G, Q, H, R)
+#%% standard discrete-time Kalman Filter (EKF)
+
+EKF = P5_classes.EKF(propogate_state, measure, gen_state_jacobian, gen_measure_jacobian, gen_process_noise_jacobian, gen_measure_noise_jacobian, Q, R)
 runFilter(EKF, filters_log['EKF'], True)
 plot(filters_log['EKF'], 'EKF')
 
-# steady-state Kalman Filter (SS_KF)
-UKF = P4_classes.UKF(F, G, Q, H, R)
-runFilter(UKF, filters_log['UKF'], True)
+
+#%% Unscented Kalman Filter (UKF)
+UKF = P5_classes.UKF(propogate_state, measure, Q, R, alpha=0.1)
+runFilter(UKF, filters_log['UKF'], False)
 plot(filters_log['UKF'], 'UKF')
 
 # plot kalman gains vs UKF gains
+'''
 fig, ax = plt.subplots()
 ax.plot(time[1:], filters_log['EKF']['gain'][:,0], c='red', label='EKF fuel remaining gain')
 ax.plot(time[1:], filters_log['EKF']['gain'][:,1], c='black', label='EKF flowmeter bias gain')
@@ -141,14 +249,17 @@ ax.set(xlabel = 't, s', ylabel = 'Kalman gains, cm^3',
       title = 'UKF vs EKF Kalman gains')
 ax.legend()
 plt.grid(True)
+'''
 
-# covariance intersection Kalman Filter (CI-EKF)
-# bound omega <= 0.95 for search stability
-BSKF = P4_classes.BSKF(F, G, Q, H, R)
+#%% bootstrap particle Kalman Filter (CI-EKF)
+
+'''
+BSKF = P5_classes.BSKF(F, G, Q, H, R)
 runFilter(BSKF, filters_log['BSKF'])
 plot(filters_log['BSKF'], 'BSKF')
+'''
 
-# fixed-interval Kalman Smoother (FI_KS)
+'''# fixed-interval Kalman Smoother (FI_KS)
 FIKS = P4_classes.FIKS(F, G, Q, H, R)
 filters_log['FIKS'] = copy.deepcopy(filters_log['UKF'])
 for i in range(time.size-2,-1,-1):
@@ -157,38 +268,9 @@ for i in range(time.size-2,-1,-1):
     x_smoothed, P_smoothed = FIKS.smooth(filters_log['FIKS']['x'][i+1,:], filters_log['UKF']['x'][i,:], uk[i], filters_log['FIKS']['P'][i+1,:,:], filters_log['UKF']['P'][i,:,:])
     filters_log['FIKS']['x'][i,:] = x_smoothed
     filters_log['FIKS']['P'][i,:,:] = P_smoothed
-plot(filters_log['FIKS'], 'FIKS')
+plot(filters_log['FIKS'], 'FIKS')'''
 
 # commentary
-'''As can be seen by the plots produced, the kalman filtering framework provides multiple
-ways to estimate states via sensor fusion. As implemented, the base kalman filter struggles to 
-track the true fuel remaining and flowmeter bias. While it appears as though this is caused 
-by too low a process noise matrix, leading to too much trust in the model 
-(shown by small covariance and out of bounds errors), the UKF child class
-performs much better with all the same equations save for the kalman gain calculation, which is constant
-and derived from the LQE DARE problem for the UKF. This is further supported by the kalman gain plot, which 
-shows that the EKF does not converge to the UKF gains. I've checked the standard kalman filter gain calculation,
-tried different expressions, and re-coded the function but haven't found the culprit for this behavior yet. 
-However, in a normal scenario the EKF should converge to the UKF and have the advantage of being able to 
-calculate the kalman gain adaptively online, leading to a more flexible and adaptive, while very similar, 
- solution as compared to the EKF. It can also be seen that the UKF approximates the covariances 
- of the states well, with the 95% bounds on both fuel remaining and meter bias containing the errors about 
- 95 percent of the time. 
- 
- Moving on, the CI-EKF can be seen to have worse performance, as it 
- struggles to fix steady state fuel remaining value with overly-small state covariances, then suddenly
- blows up in terms of covariance and erratically oscillates around the true states, seemingly 
- only chasing the measured values, discarding the prediction. This leads to very large errors, oscillations in 
- state, and covariances for most of the data window. So while the CI-EKF may be useful in 
- sensor fusion of unknown correlation or in different contexts, it appears to be a poor choice for 
- fusing this data.
- 
- Finally, the FIKS does the job of smoothing the states well, as can be seen by the much smoother output 
- as compared to the UKF data it smooths. It also entirely fixes the initial convergence period 
- the UKF has where it is initialized with a large error and small covariance, so takes a long time to converge
- to the true states. As shown in the fuel remaining plot, the FIKS tracks the true value from time zero with much
- less oscillations. However, it appears as though this is done at the cost of much increased smoothed covariance
- estimates. The FIKS has a variances of over 100 for most of the fuel remaining estimate, while the UKF
- variances remain under 50 for the same data. So it appears that the FIKS, while doing a good job of 
- smoothing the data and keeping the error similar (for this dataset, at least), decreases the confidence in the 
- accuracy of that data as estimated by the filter. '''
+'''
+A
+'''
--- P5_NonlinearKFs/P5_classes.py
+++ P5_NonlinearKFs/P5_classes.py
@@ -39,14 +39,11 @@ class BaseKF(ABC):
     # overwrite this for non-standard kalman gain calculations (SS-KF)
     def calculateKalmanGain(self, x_hat, p_hat):
         self.update_measurement_matrices(x_hat, p_hat)
-        
         self.K = p_hat @ self.H.T / (self.H @ p_hat @ self.H.T + self.R)
         
         S = self.R + self.H @ (p_hat @ self.H.T)
         self.K = np.dot(p_hat @ self.H.T, (1.0/S))
-        #S = self.R + self.H @ (p_hat @ self.H.T)
-        #self.K = np.dot(p_hat @ self.H.T, (1.0/S))
-        
+       
         '''
         
         # innovation covariance noise covariance matrix S
@@ -74,15 +71,15 @@ class BaseKF(ABC):
             self.K = p_hat @ self.H.T @ np.linalg.inv(self.S)
         pass'''
     
-    def predict(self, x_hat, p_hat, dt, u=0):
+    def predict(self, x_hat, p_hat, u=0):
        
-        self.updatePredictMatrices(x_hat) # has side effect of updating self matrices
+        self.updatePredictMatrices(x_hat) # has side effect of updating self matrices F and L
         
         # update P_hat to apriori covariance estimate P_k|k-1
         p_hat = self.F @ p_hat @ self.F.T + self.L @ self.Q @ self.L.T
         p_hat = 0.5*(p_hat + p_hat.T) # enforce symmetry
-        #epsilon = 1E-07
-        #p_hat = p_hat + np.eye(p_hat.shape[0])*epsilon
+        epsilon = 1E-07
+        p_hat = p_hat + np.eye(p_hat.shape[0])*epsilon
         
         # update predicted apriori state with nonlinear propogation equation
         x_hat = self.state_propogation(x_hat, u)
@@ -104,16 +101,21 @@ class BaseKF(ABC):
         
         # iterate here to get better measurement matrices if IEKF
 
+# FIXME this is very bad. There are four cases for the different 1 vs >1 ndim options on x and y, and
+# this does not cover them all. It is also only debugged for ndim(x)>1 and ndim(y)=1
         # Joseph form aposteriori covariance update 
-        if np.isscalar(self.K):
+        if np.isscalar(self.K): # both x and y are one dimension
             A = np.eye(p_hat.shape[0]) - self.K * self.H
-        else:
+            p_hat = A * p_hat * A + self.K * self.R * self.K
+        elif self.H.ndim == 1: # FIXME this only handles case where ndim(y)=1 and ndim(x)>1, not the other way around (that would need inner product)
+            A = np.eye(p_hat.shape[0]) - np.outer(self.K, self.H)
+            p_hat = A @ p_hat @ A.T + self.R* np.outer(self.K, self.K)
+        elif False: # this needs to be for ndim(x)=1 and ndim(y)>1
+            A = np.eye(p_hat.shape[0]) - np.inner(self.K, self.H)
+            p_hat = A * p_hat * A.T + np.inner((self.K @ self.R), self.K)
+        else: # x and y have ndim>1
             A = np.eye(p_hat.shape[0]) - self.K @ self.H
-        if np.isscalar(self.R) or np.isscalar(self.K):
-            p_hat = A @ p_hat @ A.T + self.K * self.R * self.K.T
-        else:
             p_hat = A @ p_hat @ A.T + self.K @ self.R @ self.K.T
-        
         return x_hat, p_hat
     
 class KF(BaseKF):
@@ -150,6 +152,21 @@ class KF(BaseKF):
     def update_measurement_matrices(self, x_hat, p_hat):
         # no update needed for linear KF
         pass
+    
+class SSKF(KF):
+    def calculateKalmanGain(self, x_hat, p_hat):
+        self.update_measurement_matrices(x_hat, p_hat)
+        
+        # solf for infinite horizon steady state cost P, then SS kalman gain K
+        P = scipy.linalg.solve_discrete_are(self.F.T,np.array([[self.H[0]],[self.H[1]]]),self.Q,self.R) 
+        self.K = 1/(self.H.T @ P @ self.H + self.R) * self.H.T @ P @ self.F.T
+        
+        # try number 2 (from paper)
+        D = self.H @ P @ self.H.T + self.R
+        G2 = P @ self.H.T /(D) # different from control matrix self.G
+        self.K = self.F @ G2
+    
+class CIKF(KF):
     def correct(self, y_measured, x_hat, p_hat):
         # compare expected measurement to sensor measurement
         y_hat = self.measurement(x_hat)
@@ -182,6 +199,155 @@ class KF(BaseKF):
             x_hat = x_hat + self.K @ innovation
         
         return x_hat, p_hat_posteriori
+
+class EKF(BaseKF):
+    def __init__(self, f, y, fx_jac, y_jac, process_noise_jac, measure_noise_jac, Q, R, fu_jac=0):
+        self.propogate_function = f
+        self.measure_function = y
+        self.state_jac_function = fx_jac
+        self.control_jac_function = fu_jac
+        self.measure_jac_function = y_jac
+        self.process_noise_jac_function = process_noise_jac
+        self.measure_noise_jac_function = measure_noise_jac
+        self.Q = Q
+        self.R = R
+        
+    def updatePredictMatrices(self, x_hat):
+        self.F = self.state_jac_function(x_hat)
+        self.L = self.process_noise_jac_function(x_hat)
+   
+    def state_propogation(self, x_hat, u=0):
+        return self.propogate_function(x_hat,u)
+        
+    def measurement(self, x_hat):
+        return self.measure_function(x_hat)
+    
+    def update_measurement_matrices(self, x_hat, p_hat):
+        self.H = self.measure_jac_function(x_hat) 
+        self.M = self.measure_noise_jac_function(x_hat)
+        
+class UKF():
+    def __init__(self, propogate_function, measurement_function, Q, R, alpha=1e-1, beta=2, kappa=0):
+        self.f = propogate_function # must have input args x, u, w
+        self.y = measurement_function # must have input args x, v
+        self.a = alpha
+        self.b = beta
+        self.k = kappa
+        
+        if np.isscalar(Q):
+            self.Q = np.array([Q])
+        else:
+            self.Q = Q
+        
+        if np.isscalar(R):
+            self.R = np.array([R])
+        else:
+            self.R = R
+        
+    def predict(self, x_hat, p_hat, u=0):
+        sigmas, c_weights, m_weights = self.gen_sigma_points(x_hat, p_hat)
+        UT_sigmas = np.zeros((x_hat.shape[0], sigmas.shape[1]))
+        
+        # propogata sigma points through unscented transform
+        for j in range(sigmas.shape[1]):
+            sigma_x = sigmas[:x_hat.shape[0], j]
+            sigma_w = sigmas[x_hat.shape[0] : x_hat.shape[0]+self.Q.shape[0], j]
+            UT_sigmas[:,j] = self.f(sigma_x, u, sigma_w)
+        
+        # calculate a priori mean
+        weighted_m_sigmas = UT_sigmas * m_weights
+        x_apriori = np.sum(weighted_m_sigmas, axis=1)
+        
+        # calculate apriori covariance
+        P_apriori = np.zeros((x_hat.shape[0], x_hat.shape[0]))
+        for j in range(sigmas.shape[1]):
+            dif_vector = UT_sigmas[:,j] - x_apriori
+            sigma_covariance =  np.outer(dif_vector, dif_vector)
+            P_apriori += sigma_covariance * c_weights[j]
+        
+        # log sigma points and weights in filter for measurement
+        sigmas[:x_hat.shape[0], :] = UT_sigmas
+        self.sigmas = sigmas
+        self.m_weights = m_weights
+        self.c_weights = c_weights
+        
+        return x_apriori, P_apriori
+    
+    def correct(self, y, x_apriori, P_apriori):
+        # necessary so we can take the dimension of y
+        if np.isscalar(y):
+            y = np.array([y])
+        
+        measured_sigmas = np.zeros((self.R.shape[0], self.sigmas.shape[1]))
+        
+        # propogata sigma points through measurement function
+        for j in range(self.sigmas.shape[1]):
+            sigma_x = self.sigmas[:x_apriori.shape[0], j]
+            sigma_v = self.sigmas[x_apriori.shape[0]+self.Q.shape[0]:, j]
+            measured_sigmas[:,j] = self.y(sigma_x, sigma_v)
+        
+        # calculate expected measurement
+        weighted_measurements = measured_sigmas * self.m_weights
+        y_hat = np.sum(weighted_measurements, axis=1)
+        
+        # compute innovation covariance matrix
+        P_y = np.zeros((y.shape[0], y.shape[0]))
+        for j in range(self.sigmas.shape[1]):
+            dif_vector = measured_sigmas[:,j] - y_hat
+            sigma_covariance =  np.outer(dif_vector, dif_vector)
+            P_y += sigma_covariance * self.c_weights[j]
+        
+        # compute cross-covariance matrix
+        P_xy = np.zeros((x_apriori.shape[0], y.shape[0]))
+        for j in range(self.sigmas.shape[1]):
+            dif_y = measured_sigmas[:,j] - y_hat
+            sigma_x = self.sigmas[:x_apriori.shape[0], j]
+            dif_x = sigma_x - x_apriori
+            sigma_covariance =  np.outer(dif_x, dif_y)
+            P_xy += sigma_covariance * self.c_weights[j]
+        
+        if y.shape[0] == 1:
+            K =  P_xy / (P_y)
+            x_aposteriori = x_apriori + K @ (y - y_hat)
+            P_aposteriori = P_apriori - K @ P_y @ K.T
+        else:
+            K = P_xy @ np.linalg.inv(P_y)
+            x_aposteriori = x_apriori + K @ (y - y_hat)
+            P_aposteriori = P_apriori - K @ P_y @ K.T
+        
+        # log kalman gain for fun
+        self.K = K
+        
+        return x_aposteriori, P_aposteriori
+        
+    def gen_sigma_points(self, x_hat, p_hat):
+        
+        # define augmented state and covariance matrix
+        dim_sigma = x_hat.shape[0] + self.Q.shape[0] + self.R.shape[0]
+        sigmas = np.zeros((dim_sigma, 2*dim_sigma + 1))
+        m_weights = np.zeros(sigmas.shape[1])
+        c_weights = np.zeros(sigmas.shape[1])
+        P_aug = scipy.linalg.block_diag(p_hat, self.Q, self.R)
+        lam = self.a**2*(dim_sigma + self.k) - dim_sigma
+        
+        # calculate central sigma point and weights
+        sigma0 = np.block([x_hat, np.zeros(sigmas.shape[0] - x_hat.shape[0])])
+        sigmas[:,0] = sigma0
+        m_weights[0] = lam/(dim_sigma + lam)
+        c_weights[0] = lam/(dim_sigma + lam) + 1 - self.a**2 + self.b
+        
+        # calculate all other sigma points and weights
+        for j in range(1, dim_sigma+1):
+            root_P = np.linalg.cholesky((dim_sigma + lam )*P_aug) # FIXME understand why taking root of row instead of this fucked the filter 
+            displacement = root_P[j-1,:]
+            sigmas[:,j] = sigma0 + displacement
+            sigmas[:,j+dim_sigma] = sigma0 - displacement
+            m_weights[j] = 1/(2*(dim_sigma + lam))
+            m_weights[j+dim_sigma] = m_weights[j]
+            c_weights[j] = m_weights[j]
+            c_weights[j+dim_sigma] = m_weights[j]
+            
+        return sigmas, c_weights, m_weights
     
 class FIKS(KF):
     # this assumes u is a scalar
